d6c2c69ac600cebc500dce4527c349337bd184f7
Clayton Chew <30117799+claytonchew@users.noreply.github.com>
2025-12-31 18:23:41 +0800

    chore: introduce slight delay to ensure turso is ready

--- workers/database-manager/src/plugins/main.ts
+++ workers/database-manager/src/plugins/main.ts
@@ -1,5 +1,8 @@
+import { sleep } from "@nidoz/utils";
+
 export default defineNitroPlugin(async () => {
 	try {
+		await sleep(2000); // wait for the database to be ready
 		await useMigrator().migrate();
 		await useSeeder().run();
 		console.info("Database migration and seeding completed!");
