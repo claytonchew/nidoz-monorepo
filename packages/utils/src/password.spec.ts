8c84ef3ab1a46ffbad8203116fc19baebb9c9c8e
Clayton Chew <30117799+claytonchew@users.noreply.github.com>
2026-01-02 16:11:33 +0800

    chore: add no-op to password test

--- packages/utils/src/password.spec.ts
+++ packages/utils/src/password.spec.ts
@@ -1,8 +1,11 @@
-import { describe } from "vitest";
+import { describe, it } from "vitest";
 import { hashPassword } from "./password";
 
 describe("generate password", async () => {
-	const password = "nidozmo88";
+	const email = "nidozmgmt@gmail.com";
+	const password = "Nidozmgmt1*";
+	console.log(email);
 	console.log(await hashPassword(password));
-	// $scrypt$n=16384,r=8,p=1$XarGzD4uqtREcVqyNFszdw$+FPYBDQqhO8IAwHvaYuj1Miwn3qlISodDDlISk1H3jbO3gygQLS27vv3IlPSrsofoslVOw1jp8dLjM6hly54wQ
+
+	it("no-op", () => {});
 });
